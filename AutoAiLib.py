f016f1cfb61a5671f91323a13aaa274a75cffb26
Matthew Mulhall <32942111+mattlm0831@users.noreply.github.com>
2019-08-06 16:29:37 -0400

    Some changes to file structure.

--- AutoAiLib.py
+++ AutoAiLib.py
@@ -7,13 +7,18 @@ Created on Tue Aug  6 09:35:27 2019
 import os
 import random
 import shutil
-from img_transform import transform_many
 import progressbar
 import pandas as pd
 from keras import models as m
 from keras.preprocessing.image import img_to_array
 import numpy as np
 import cv2
+from scipy import ndarray
+from scipy import ndimage
+import skimage as sk
+from skimage import transform
+from skimage import util
+
 
 def check_right(row):
     if row['file'] == row['prediction']:
@@ -183,3 +188,84 @@ def place_images(ROOT_DIR, original_data_dir, train=.7, validation=.2, test=.1):
     bar.finish()
     print("\nMoved ", total, ' images.')
      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #IMAGE UTILS
+def random_rotation(image_array: ndarray):
+    rand_degree = random.uniform(-25, 25)
+    return transform.rotate(image_array, rand_degree)
+
+def random_noise(image_array: ndarray):
+    return util.random_noise(image_array)
+
+def horizontal_flip(image_array: ndarray):
+    return image_array[:, ::-1]
+
+#def scale_out(image_array: ndarray):
+#    factor = random.uniform(1, 2)
+#    return transform.rescale(image_array, scale=factor, mode='constant')
+#
+#def scale_in(image_array: ndarray):
+#    
+#    return transform.rescale(image_array, scale=.5, mode='constant')
+
+def blur(image_array: ndarray):
+    return ndimage.uniform_filter(image_array, size=image_array.shape)
+
+
+avail_transforms = {
+        'rotate' : random_rotation,
+        'noise' : random_noise,
+        'horizontal-flip' : horizontal_flip
+        }
+def transform_many(folder, num_files_desired = 300):
+    sub_folders = [os.path.join(folder, f) for f in os.listdir(folder)]
+    
+    bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
+    val = 0
+    for folder in sub_folders:
+        images = [os.path.join(folder, pic) for pic in os.listdir(folder)]
+        
+        generated = 0
+        
+        while generated <= num_files_desired:
+            bar.update(val)
+            image = random.choice(images)
+            
+            imgarray = sk.img_as_float(sk.io.imread(image))
+            num_transforms = 0
+            t = random.randint(1, len(avail_transforms))
+            while num_transforms <= t:
+                key = random.choice(list(avail_transforms))
+                imgarray = avail_transforms[key](imgarray)
+                num_transforms+=1
+            
+            generated+=1
+            name =  'augmented_' + str(generated) + '.png'
+            newfile= os.path.join(folder, name)
+            
+            sk.io.imsave(newfile, imgarray)
+            val+=1
+        
+    bar.finish()
+    print('\nGenerated ', val, ' images.')
+    
+    
+    
+    
+    
---
+++ testing/testing.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 15:58:46 2019
+
+@author: matth
+"""
+import AutoAiLib as ai
+
+ai.compile_data(r'C:\Users\matth\Desktop\ascii_lower', r'C:\Users\matth\Desktop\test', num_imgs_per_class=200)
