6d5bba6b5d09c1c762019229fd83fe204c64e518
Matthew Mulhall <32942111+mattlm0831@users.noreply.github.com>
2019-08-06 12:52:37 -0400

    Small changes

---
+++ setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name="AutoAI",
+      version ='0.1dev',
+      packages=['AutoAI',],
+      license='GNU GPLv3',
+      long_description=open('README.md').read())
